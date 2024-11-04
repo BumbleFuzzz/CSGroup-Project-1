9d8c64bab49f30af565744c34b604e1ee3777871
napier13 <napier13@purdue.edu>
2024-11-03 19:42:32 -0500

    Update UserDatabaseInterface.java

--- UserDatabaseInterface.java
+++ UserDatabaseInterface.java
@@ -1,6 +1,13 @@
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * Interface corresponding to the UserDatabase class
+ *
+ * @author Austin Napier
+ * @version November 3, 2024
+ */
+
 public interface UserDatabaseInterface {
     public void addUser(User user);
     public void removeUser(User user);
