e1cd8207cc5805b9b48fa96bb19b4fdd059620e8
BumbleFuzzz <acemon2006@gmail.com>
2024-11-07 18:38:05 -0500

    Created a comments interface

---
+++ CommentsInterface.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+
+/**
+ * Interface corresponding to the Comments class.
+ *
+ *
+ * @author Asher Earnhart
+ * @version November 7, 2024
+ */
+
+public interface CommentsInterface {
+
+    // Appends the comment to the associated post's file
+    void appendCommentToPostFile() throws IOException;
+
+    // Increments the comment's upvote count
+    void upvote();
+
+    // Increments the comment's downvote count
+    void downvote();
+
+    // Deletes the comment from the associated post's file
+    void deleteCommentFromPostFile(User commenter) throws IOException;
+
+    // Getter for the comment's text
+    String getCommentText();
+
+    // Getter for the comment's author
+    User getCommenter();
+
+    // Getter for the comment's associated post
+    PostClass getPost();
+
+    // Getter for the comment's upvote count
+    int getUpvotes();
+
+    // Getter for the comment's downvote count
+    int getDownvotes();
+}
