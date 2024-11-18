c94be75eac9ad99626377df836532c559fa41aa6
yogtrivedi <trivediy@purdue.edu>
2024-11-17 23:17:34 -0500

    Update ClientHandlerInterface.java

--- ClientHandlerInterface.java
+++ ClientHandlerInterface.java
@@ -1,5 +1,12 @@
 import java.io.IOException;
 
+/**
+ * Interface for the ClientHandler class
+ *
+ * @author Yog Trivedi
+ * @version November 17, 2024
+ */
+
 public interface ClientHandlerInterface extends Runnable {
     
     void run();
