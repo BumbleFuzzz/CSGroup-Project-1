db9b9f851bb4b8dbb2ff1824e4c0b422b9b5811a
yogtrivedi <trivediy@purdue.edu>
2024-11-17 23:13:48 -0500

    Update ClientInterface.java

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 /**
- * Interface for the ClientHandler class
+ * Interface for the Client class
  *
  * @author Yog Trivedi
  * @version November 17, 2024
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public interface ClientInterface {
 
+    Object sendRequest(Object request);
     void closeConnection();
 
 }
