8bac9c08b06685c0aaa41bac3e38f8c2852fcf3c
napier13 <napier13@purdue.edu>
2024-11-17 23:06:31 -0500

    Added Javadoc

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,5 +1,12 @@
 import java.io.ObjectOutputStream;
 
+/**
+ * Interface for the Server class
+ *
+ * @author Yog Trivedi
+ * @version November 17, 2024
+ */
+
 public interface ServerInterface {
     void startServer(int port);
     void stopServer();
