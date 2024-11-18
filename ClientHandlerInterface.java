28e51608a76e0f81eda112da8cd65547e81d4d1b
yogtrivedi <trivediy@purdue.edu>
2024-11-17 23:17:03 -0500

    Update ClientHandlerInterface.java

--- ClientHandlerInterface.java
+++ ClientHandlerInterface.java
@@ -1,19 +1,12 @@
 import java.io.IOException;
 
-/**
- * Interface for the ClientHandler class
- *
- * @author Yog Trivedi
- * @version November 17, 2024
- */
-
 public interface ClientHandlerInterface extends Runnable {
-
-
-    void initializeStreams() throws IOException;
     
-    void handleClientRequests() throws IOException, ClassNotFoundException;
+    void run();
+
+    void sendResponse(Object response) throws IOException;
+
+    Object readRequest() throws IOException, ClassNotFoundException;
 
     void closeConnection() throws IOException;
-
 }
