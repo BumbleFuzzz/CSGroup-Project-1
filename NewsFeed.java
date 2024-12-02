b660db1ce30f59137e17f9d378bf1861fb194222
yogtrivedi <trivediy@purdue.edu>
2024-12-02 16:31:09 -0500

    formatted properly NewsFeed.java

--- NewsFeed.java
+++ NewsFeed.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -8,8 +10,7 @@ import java.util.ArrayList;
  * @version November 17, 2024
  */
 
-public class NewsFeed implements NewsFeedInterface
-{
+public class NewsFeed implements NewsFeedInterface {
     private ArrayList<String> allPosts;
     // All posts in the feed
 
@@ -19,82 +20,65 @@ public class NewsFeed implements NewsFeedInterface
     private ArrayList<String> hiddenPosts;
     // Posts from blocked users
 
-    public NewsFeed()
-    {
+    public NewsFeed() {
         allPosts = new ArrayList<>();
         friendPosts = new ArrayList<>();
         hiddenPosts = new ArrayList<>();
     }
 
     // Reads posts from a given file and displays them to the feed
-    public void readPostsFromFile(String fileName)
-    {
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
-        {
+    public void readPostsFromFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String post;
-            while ((post = br.readLine()) != null)
-            {
+            while ((post = br.readLine()) != null) {
                 allPosts.add(post);
             }
             displayFeed();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     // Shows all posts in the feed
-    public void displayFeed()
-    {
+    public void displayFeed() {
         System.out.println("Here is your News Feed!");
-        for (String post : allPosts)
-        {
-            if (!hiddenPosts.contains(post))
-            {
+        for (String post : allPosts) {
+            if (!hiddenPosts.contains(post)) {
                 System.out.println(post);
             }
         }
     }
 
     // Adds a post to friendPosts
-    public void addFriendPost(String post)
-    {
-        if (!friendPosts.contains(post))
-        {
+    public void addFriendPost(String post) {
+        if (!friendPosts.contains(post)) {
             friendPosts.add(post);
         }
     }
 
     // Hides a post
-    public void hidePost(String post)
-    {
-        if (!hiddenPosts.contains(post))
-        {
+    public void hidePost(String post) {
+        if (!hiddenPosts.contains(post)) {
             hiddenPosts.add(post);
         }
     }
 
     // Makes a post observable after being hidden
-    public void disclosePost(String post)
-    {
+    public void disclosePost(String post) {
         hiddenPosts.remove(post);
     }
 
     // Getters for each respective type of post
 
-    public ArrayList<String> getAllPosts()
-    {
+    public ArrayList<String> getAllPosts() {
         return allPosts;
     }
 
-    public ArrayList<String> getFriendPosts()
-    {
+    public ArrayList<String> getFriendPosts() {
         return friendPosts;
     }
 
-    public ArrayList<String> getHiddenPosts()
-    {
+    public ArrayList<String> getHiddenPosts() {
         return hiddenPosts;
     }
 }
